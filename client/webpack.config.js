1f6f795122d32cfefad6a495d092dafcc8ad3abb
Lex Matthews <tanukiguernica@gmail.com>
2022-05-26 09:04:10 -0400

    added plugins and rules to webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,12 +18,51 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Text Editor'
+      }),
+      InjectManifest({
+        swSrc: "src-sw.js",
+        swDest: "service-worker.js"
+      }),
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Text Editor',
+        short_name: 'Editor',
+        description: 'Save and modify all text documents!',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ]
+      })
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/i,
+          exclude: /node_modules/,
+          use:{
+            loader: 'babel-loader',
+            options:{
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime']
+            }
+          }
+        },
       ],
     },
   };
